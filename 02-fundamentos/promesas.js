950293d43aa112f0abdf6acede88032b99d9e7f8
eddypaz <eddypaz150893@gmail.com>
2019-09-15 00:35:13 -0600

    14sep

--- 02-fundamentos/promesas.js
+++ 02-fundamentos/promesas.js
@@ -29,8 +29,36 @@ let getEmpleado=(id)=>{
 	});	
 }
 
-getEmpleado(10).then( empleado => {
-	console.log('Empleado de la BD', empleado);
-},(err) =>{
+let getSalario = (empleado)=>{
+	return new Promise( (resolve, reject)=>{
+		let salarioDB=salarios.find( salario => salario.id === empleado.id)
+	if(!salarioDB){
+		reject(`No se encontro un salario para el usuario ${ empleado.nombre}`);
+	}else{
+		resolve({
+			nombre:empleado.nombre,
+			salario:salarioDB.salario,
+			id: empleado.id});
+	}
+	})
+}
+// getEmpleado(3).then( empleado => {/*then es el resolve, err es el reject*/
+// 	console.log('Empleado de la BD', empleado);
+
+// 	getSalario(empleado).then( resp => {
+// 		console.log(`El salario de ${ resp.nombre } es de ${ resp.salario }$`);
+// 	},(err)=>{
+// 		console.log(err);
+// 	})
+// },(err) =>{
+// 	console.log(err);
+// });
+getEmpleado(5).then( empleado =>{
+	return getSalario(empleado);
+})
+.then(resp =>{
+	console.log(`El salario de ${ resp.nombre } es de ${ resp.salario} `);
+})
+.catch(err =>{
 	console.log(err);
-});
+})
